ae5305dfb95d52f50a44fd0591f496434c18c6aa
SnoopyCafe <presto.williams@gmail.com>
2017-01-09 17:57:53 -0500

    test

--- python/machine_learning/demo_pandas.py
+++ python/machine_learning/demo_pandas.py
@@ -3,11 +3,20 @@ import pandas as pd
 names = ['student1','student2','student3','student4','student5']
 grades_pct = [80,75,65,87,92]
 studentDataset = list(zip(names,grades_pct))
-print (studentDataset)
+
+print (studentDataset, "\n")
 
 # Data Frame
 df = pd.DataFrame(data=studentDataset, columns=['Names','grades_pct'])
+print (df, "\n")
 
-path = 'student_data.csv'
-df.to_csv(path)
-print (df)
+# Export to CSV
+# path = 'student_data.csv'
+# df.to_csv(path)
+
+# Sorting
+print (df.sort_values(['grades_pct'],ascending=True), "\n")
+
+# Dataset stats
+print (df.describe())
+
