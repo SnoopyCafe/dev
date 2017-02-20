081b464a4db742b22a814a71109566eaaecad87d
Preston Williams <presto.williams@gmail.com>
2017-02-20 13:46:58 -0500

    added mini-batch initial

--- python/udacity/color_select.py
+++ python/udacity/color_select.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
 
+
 # Read in the image
 image = mpimg.imread('test.jpg')
 
--- python/udacity/tensorflow/test_accuracy.py
+++
@@ -1,5 +0,0 @@
-__author__ = 'admin'
-a = 1
-for i in range(1000000):
-    a = a + 1e-6
-print(a - 1000000000)
